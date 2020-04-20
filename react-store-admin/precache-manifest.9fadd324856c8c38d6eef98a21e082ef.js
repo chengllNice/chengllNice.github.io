ede8b50a699f6cc025af6c4542a5253b001ced09
chenglulu <919903021@qq.com>
2020-04-20 16:56:53 +0800

    添加react-store-admin

---
+++ react-store-admin/precache-manifest.9fadd324856c8c38d6eef98a21e082ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96e35dc65808ad9fda34bba354bad9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edcbffa5fae7c9477ca2",
+    "url": "/static/css/2.715b4953.chunk.css"
+  },
+  {
+    "revision": "96e08d49045d5d67405c",
+    "url": "/static/css/main.281f09b1.chunk.css"
+  },
+  {
+    "revision": "edcbffa5fae7c9477ca2",
+    "url": "/static/js/2.6e8459f7.chunk.js"
+  },
+  {
+    "revision": "96e08d49045d5d67405c",
+    "url": "/static/js/main.dbb59703.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9094f451a789baeb33c9ec0d9b44394a",
+    "url": "/static/media/login-bg.9094f451.jpg"
+  },
+  {
+    "revision": "d9dc09a779e9b8e1499dc68932c031c0",
+    "url": "/static/media/map-marker.d9dc09a7.png"
+  }
+]);

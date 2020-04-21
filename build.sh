be663a1598c8d3b2b4d4bcd8d3ed2429f75d1cee
chenglulu <919903021@qq.com>
2020-04-21 10:55:03 +0800

    react

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "build environment：$1";
+echo "push project：$2";
+
+if [[ $1 == "online" ]]
+then
+    echo "git autopush start..."
+    git add .
+    git commit -m $2
+    # git checkout master
+    git push origin master
+    echo "git autopush end..."
+else
+    echo "environment is invalid"
+fi
+
+echo "end build ==============="
